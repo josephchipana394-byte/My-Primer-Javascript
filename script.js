3b60efa11a6fdcf48829148c955a4079acf0d0ea
josephchipana394-byte <josephchipana394@gmail.com>
2026-06-17 00:29:30 -0500

    Probando promt Alert y concatenacion moderna usando variables y ademas operaciones basicas

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+//declarando variables
+let num_uno=10;
+let num_dos=4;
 let nombre="Joseph Chipana";
-let edad=prompt("Ingrese su edad");
-let ciudad=prompt("Ingrese una Ciudad")
 let distrito="SJL"
 let programador=true;
+// probando la funcion prompt
+let edad=prompt("Ingrese su edad");
+let ciudad=prompt("Ingrese una Ciudad")
 
-
+//impresion de variables con cada tipo de dato
 console.log(nombre)
 console.log(typeof nombre)
 console.log(edad)
@@ -16,8 +20,21 @@ console.log(typeof distrito)
 console.log(programador)
 console.log(typeof programador)
 
+// concatenacion moderna
 let mensaje=`Hola mi nombre es ${nombre} tengo ${edad} soy de la ciudad de ${ciudad} del distrito de ${distrito} y soy programdor ${programador}`;
 
+//mostrando mensaje con la funcion Alert
 alert(mensaje)
 console.log(mensaje)
 
+//Operaciones basicas en consola
+console.log("Mostrando Operaciones basicas +,-,*,/")
+console.log("-------------------------------------")
+let suma = num_uno+num_dos;
+let resta = num_uno-num_dos;
+let mult = num_uno*num_dos;
+let division = num_uno/num_dos;
+console.log(`la suma de: ${Number(num_uno)} + ${Number(num_dos)} = ${suma}`)
+console.log(`la suma de: ${Number(num_uno)} - ${Number(num_dos)} = ${resta}`)
+console.log(`la suma de: ${Number(num_uno)} * ${Number(num_dos)} = ${mult}`)
+console.log(`la suma de: ${Number(num_uno)} / ${Number(num_dos)} = ${division}`)
