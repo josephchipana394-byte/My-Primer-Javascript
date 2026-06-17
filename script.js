2677c665d8fce9f744a41225e0a49888ec63accf
josephchipana394-byte <josephchipana394@gmail.com>
2026-06-17 00:05:50 -0500

    Probando promt Alert y concatenacion moderna usando variables

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let nombre="Angel Paz"
+let nombre="Joseph Chipana";
+let edad=prompt("Ingrese su edad");
+let ciudad=prompt("Ingrese una Ciudad")
+let distrito="SJL"
+let programador=true;
+
+
 console.log(nombre)
 console.log(typeof nombre)
+console.log(edad)
+console.log(typeof edad)
+console.log(ciudad)
+console.log(typeof ciudad)
+console.log(distrito)
+console.log(typeof distrito)
+console.log(programador)
+console.log(typeof programador)
 
-let numero1=3
-let numero2=5
-console.log(3+5)
+let mensaje=`Hola mi nombre es ${nombre} tengo ${edad} soy de la ciudad de ${ciudad} del distrito de ${distrito} y soy programdor ${programador}`;
+
+alert(mensaje)
+console.log(mensaje)
+
